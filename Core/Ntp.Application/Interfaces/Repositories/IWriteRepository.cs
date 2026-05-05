beb13349d3902bd8b879f54f968c8a9510244149
thesincar <thesincar@gmail.com>
2026-05-05 11:51:31 +0300

    Ders 6 dosyaları tamamlandı

---
+++ Core/Ntp.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ntp.Application.Interfaces.Repositories;
+
+internal interface IWriteRepository
+{
+}
--- Infrastructure/Ntp.Persistance/Registration.cs
+++ Infrastructure/Ntp.Persistance/Registration.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ntp.Application.Interfaces.Repositories;
 using Ntp.Persistance.Context;
+using Ntp.Persistance.Repositories;
 
 namespace Ntp.Persistance;
 
@@ -10,5 +12,7 @@ public static class Registration
     public static void AddPersistance(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
     }
 }
